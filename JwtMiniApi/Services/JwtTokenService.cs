9588620e262c5c4258be3eeb35bac33602dfb4be
Süphan <yakutsuphan@gmail.com>
2026-01-22 14:35:45 +0100

    add JwtTokenService

---
+++ JwtMiniApi/Services/JwtTokenService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtMiniApi.Services
+{
+    public class JwtTokenService
+    {
+        private readonly IConfiguration _config;
+
+        public JwtTokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public (string token, DateTime expiresAt) CreateToken(string username)
+        {
+            var jwt = _config.GetSection("Jwt");
+            var key = jwt["Key"]!;
+            var issuer = jwt["Issuer"]!;
+            var audience = jwt["Audience"]!;
+            var expiresMinutes = int.Parse(jwt["ExpiresMinutes"]!);
+
+            var expiresAt = DateTime.UtcNow.AddMinutes(expiresMinutes);
+
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Name, username)
+        };
+
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var creds = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: expiresAt,
+                signingCredentials: creds
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return (tokenString, expiresAt);
+        }
+    }
+}
